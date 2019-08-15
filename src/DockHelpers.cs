823432a2a36762df9b426fb6a1fe5c1b078290e7
Mads Kristensen <madskristensen@users.noreply.github.com>
2019-08-15 14:01:05 -0700

    Refactorings

---
+++ src/DockHelpers.cs
@@ -0,0 +1,37 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+
+namespace ResetToolWindows
+{
+    public static class DockHelpers
+    {
+        public static void DockToolWindows(DTE2 dte)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            dte.ExecuteCommand("Window.AutoHideAll");
+
+            ToolWindows windows = dte.ToolWindows;
+            GeneralOptions options = GeneralOptions.Instance;
+
+            Dock(windows.CommandWindow.Parent, options.ShowCommandWindow);
+            Dock(windows.ErrorList.Parent, options.ShowErrorList);
+            Dock(windows.OutputWindow.Parent, options.ShowOutputWindow);
+            Dock(windows.SolutionExplorer.Parent, options.ShowSolutionExplorer);
+            Dock(windows.TaskList.Parent, options.ShowTaskList);
+            Dock(windows.ToolBox.Parent, options.ShowToolbox);
+        }
+
+        private static void Dock(Window windowToDock, bool shouldDock)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (shouldDock && windowToDock.Visible)
+            {
+                // This docks the windows to their last docked location
+                windowToDock.IsFloating = false;
+            }
+        }
+    }
+}
--- src/ResetToolWindowsPackage.cs
+++ src/ResetToolWindowsPackage.cs
@@ -1,12 +1,9 @@
 ﻿using EnvDTE;
 using EnvDTE80;
-using Microsoft;
 using Microsoft.VisualStudio.Shell;
 using System;
 using System.Runtime.InteropServices;
-using System.Threading;
 using static Microsoft.VisualStudio.VSConstants;
-using Task = System.Threading.Tasks.Task;
 
 namespace ResetToolWindows
 {
@@ -17,30 +14,14 @@ namespace ResetToolWindows
     [Guid("f2156abf-b775-442c-ab74-92c77551c474")]
     public sealed class ResetToolWindowsPackage : AsyncPackage
     {
-        private DTE2 _dte;
-
-        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-        {
-            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-
-            _dte = await GetServiceAsync(typeof(DTE)) as DTE2;
-            Assumes.Present(_dte);
-        }
-
         protected override int QueryClose(out bool canClose)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
             try
             {
-                _dte.ExecuteCommand("Window.AutoHideAll");
-
-                DockWindow(GeneralOptions.Instance.ShowCommandWindow, _dte.ToolWindows.CommandWindow.Parent);
-                DockWindow(GeneralOptions.Instance.ShowErrorList, _dte.ToolWindows.ErrorList.Parent);
-                DockWindow(GeneralOptions.Instance.ShowOutputWindow, _dte.ToolWindows.OutputWindow.Parent);
-                DockWindow(GeneralOptions.Instance.ShowSolutionExplorer, _dte.ToolWindows.SolutionExplorer.Parent);
-                DockWindow(GeneralOptions.Instance.ShowTaskList, _dte.ToolWindows.TaskList.Parent);
-                DockWindow(GeneralOptions.Instance.ShowToolbox, _dte.ToolWindows.ToolBox.Parent);
+                var dte = GetService(typeof(DTE)) as DTE2;
+                DockHelpers.DockToolWindows(dte);
             }
             catch (Exception ex)
             {
@@ -48,17 +29,6 @@ namespace ResetToolWindows
             }
 
             return base.QueryClose(out canClose);
-        }
-
-        private void DockWindow(bool shouldShow, Window windowToDock)
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
-
-            if (shouldShow && windowToDock.Visible)
-            {
-                windowToDock.IsFloating = false;
-                windowToDock.Visible = true;
-            }
         }
     }
 }
